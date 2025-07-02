e68f221f543ba53da53689a2847988fb08381cfa
binumerm1 <anbinumer@users.noreply.github.com>
2025-07-02 22:28:46 +1000

    Update project files

--- lti/urls.py
+++ lti/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('config/', views.lti_config, name='lti_config'),
     path('login/', views.lti_login, name='lti_login'),
     path('launch/', views.lti_launch, name='lti_launch'),
     path('jwks/', views.lti_jwks, name='lti_jwks'),
