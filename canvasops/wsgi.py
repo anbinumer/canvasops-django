d6980f0c3534a6ca41d0466dc0984202f7c01ead
binumerm1 <anbinumer@users.noreply.github.com>
2025-07-03 11:57:14 +1000

    Write private.key from PRIVATE_KEY_B64 env var at startup

--- canvasops/wsgi.py
+++ canvasops/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import base64
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'canvasops.settings')
+
+private_key_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'private.key'))
+if not os.path.exists(private_key_path) and os.environ.get('PRIVATE_KEY_B64'):
+    with open(private_key_path, 'wb') as f:
+        f.write(base64.b64decode(os.environ['PRIVATE_KEY_B64']))
 
 application = get_wsgi_application()
